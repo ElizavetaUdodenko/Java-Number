fcfa43d91557d19c5ca5964af3b7b1a851a5efda
Elizabeth Udodenko <eli.udodenko@gmail.com>
2020-01-28 14:32:23 +0300

    Number class added

---
+++ src/Number.java
@@ -0,0 +1,19 @@
+public class Number {
+    public static void main(String[] args) {
+
+        String s1 = "min value of number = " , s2 = "max value of number = " ;
+        System.out.println(s1 + "(byte) " + Byte.MIN_VALUE);
+        System.out.println(s2 + "(byte) " + Byte.MAX_VALUE);
+        System.out.println(s1 + "(short) " + Short.MIN_VALUE);
+        System.out.println(s2 + "(short) " + Short.MAX_VALUE);
+        System.out.println(s1 + "(int) " + Integer.MIN_VALUE);
+        System.out.println(s2 + "(int) " + Integer.MAX_VALUE);
+        System.out.println(s1 + "(long) " + Long.MIN_VALUE);
+        System.out.println(s2 + "(long) " + Long.MAX_VALUE);
+        System.out.println(s1 + "(float) " + Float.MIN_VALUE);
+        System.out.println(s2 + "(float) " + Float.MAX_VALUE);
+        System.out.println(s1 + "(double) " + Double.MIN_VALUE);
+        System.out.println(s2 + "(double) " + Double.MAX_VALUE);
+
+    }
+}
